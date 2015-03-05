06ed0ec9b4c0d1fb46c32d22344dcd76a2012a36
MKS-Student <mks.student@hackreactor.com>
2015-03-05 11:35:38 -0600

    completes queue pseudoclassical; momentum still intact

--- sprint-one/src/pseudoclassical/queue.js
+++ sprint-one/src/pseudoclassical/queue.js
@@ -1,6 +1,26 @@
 var Queue = function() {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
+  this.storage = {}
+  this.start = -1
+  this.end = -1
 };
+
+Queue.prototype.enqueue = function(value){
+  this.end++
+  this.storage[this.end] = value
+}
+
+Queue.prototype.dequeue = function(){
+  this.end - this.start && this.start++
+  var result = this.storage[this.start]
+  delete this.storage[this.start]
+  return result
+}
+
+Queue.prototype.size = function(){
+  return this.end - this.start
+}
+
 
 

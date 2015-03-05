a26dfb9292e894f5b8114aacd29f533bfa00987f
MKS-Student <mks.student@hackreactor.com>
2015-03-05 09:50:51 -0600

    completes stack for prototypal patter

--- sprint-one/src/prototypal/stack.js
+++ sprint-one/src/prototypal/stack.js
@@ -1,8 +1,28 @@
 var Stack = function() {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
+  var newStack = Object.create(stackMethods)
+  newStack.storage = {}
+  newStack.length = 0
+  return newStack
 };
 
 var stackMethods = {};
+
+stackMethods.push = function(value){
+	this.storage[this.length] = value
+	this.length++
+}
+
+stackMethods.pop = function(){
+	this.length && this.length--
+	var results = this.storage[this.length]
+	delete this.storage[this.length]
+	return results
+}
+
+stackMethods.size = function(){
+	return this.length
+}
 
 

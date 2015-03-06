4cb21319ac314dd1787b389e82136a26e69f052c
MKS-Student <mks.student@hackreactor.com>
2015-03-06 12:44:06 -0600

    passes 4 out of 6 tests

--- sprint-two/src/graph.js
+++ sprint-two/src/graph.js
@@ -1,24 +1,35 @@
 
 
 var Graph = function(){
+  this.storage = {}
 };
 
 Graph.prototype.addNode = function(node){
+  var result = {}
+  result.edges = {}
+  this.storage[node] = result
 };
 
 Graph.prototype.contains = function(node){
+  return _.contains(Object.keys(this.storage), node)
 };
 
 Graph.prototype.removeNode = function(node){
+  delete this.storage[node]
 };
 
 Graph.prototype.hasEdge = function(fromNode, toNode){
+  return _.contains(Object.keys(this.storage[fromNode]["edges"]), toNode)
 };
 
 Graph.prototype.addEdge = function(fromNode, toNode){
+  debugger
+  this.storage[fromNode]["edges"][toNode] = this.storage[toNode]
+  this.storage[toNode]["edges"][fromNode] = this.storage[fromNode]
 };
 
 Graph.prototype.removeEdge = function(fromNode, toNode){
+
 };
 
 Graph.prototype.forEachNode = function(cb){

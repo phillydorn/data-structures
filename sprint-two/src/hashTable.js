059e670d9f9906c5fcb024a31c35562d1f92a622
MKS-Student <mks.student@hackreactor.com>
2015-03-06 16:06:37 -0600

    Implements hash table

--- sprint-two/src/hashTable.js
+++ sprint-two/src/hashTable.js
@@ -5,15 +5,32 @@ var HashTable = function(){
 
 HashTable.prototype.insert = function(k, v){
   var i = getIndexBelowMaxForKey(k, this._limit);
+  var inBucket = [k,v]
+  if (!Array.isArray(this._storage[i])) {
+    this._storage[i]=[]
+  }
+  this._storage[i].push(inBucket)
 };
 
 HashTable.prototype.retrieve = function(k){
   var i = getIndexBelowMaxForKey(k, this._limit);
-
+  var bucket = this._storage[i]
+  for (var j = 0; j < bucket.length; j++) {
+    if (bucket[j][0] === k) {
+      return bucket[j][1]
+    }
+  }
 };
 
 HashTable.prototype.remove = function(k){
-
+  var i = getIndexBelowMaxForKey(k, this._limit);
+  var bucket = this._storage[i]
+  for (var j = 0; j < bucket.length; j++) {
+    if (bucket[j][0] === k) {
+      this._storage[i][j][1] = null
+      // this._storage[i] = bucket.slice(0, j) + bucket.slice(j + 1, bucket.length)
+    }
+  }
 };
 
 

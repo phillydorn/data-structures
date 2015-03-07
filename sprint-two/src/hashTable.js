e18537a6c0099c113ad63291bf8dfba2595810f9
MKS-Student <mks.student@hackreactor.com>
2015-03-07 17:28:16 -0600

    refactored hashTable to resize. it was difficult

--- sprint-two/src/hashTable.js
+++ sprint-two/src/hashTable.js
@@ -1,6 +1,7 @@
 var HashTable = function(){
   this._limit = 8;
   this._storage = LimitedArray(this._limit);
+  this._count = 0
 };
 
 HashTable.prototype.insert = function(k, v){
@@ -8,7 +9,6 @@ HashTable.prototype.insert = function(k, v){
   var tuple = [k,v]
   var bucket = this._storage.get(i)
   var found = false
-  debugger
   if (!Array.isArray(bucket)){
     bucket = []
     this._storage.set(i, bucket)
@@ -20,26 +20,59 @@ HashTable.prototype.insert = function(k, v){
       break
     }
   }
-  found || bucket.push(tuple)
+  if (!found) {
+    bucket.push(tuple)
+    this._count++
+  }
+  if (this._count > this._limit*0.75) {
+    this._limit*= 2
+   this.resize(this._limit)
+  }
 };
+
+HashTable.prototype.resize = function(limit) {
+    var oldStorage = this._storage
+    this._count = 0
+    this._storage = LimitedArray(limit)
+    var context = this
+    oldStorage.each(function (bucket, index, collection){
+      if (bucket !== undefined){
+        for (var t = 0; t< bucket.length; t++) {
+          if (bucket[t][1] !== null){
+            context.insert(bucket[t][0],bucket[t][1])
+          }
+        }  
+      }
+    })
+}
 
 HashTable.prototype.retrieve = function(k){
   var i = getIndexBelowMaxForKey(k, this._limit);
   var bucket = this._storage.get(i)
-  for (var j = 0; j < bucket.length; j++) {
-    if (bucket[j][0] === k) {
-      return bucket[j][1]
+  if (bucket !== undefined){
+    for (var j = 0; j < bucket.length; j++) {
+      if (bucket[j][0] === k) {
+        return bucket[j][1]
+      }
     }
+  } else {
+    return null
   }
 };
 
 HashTable.prototype.remove = function(k){
   var i = getIndexBelowMaxForKey(k, this._limit);
   var bucket = this._storage.get(i)
+  debugger
   for (var j = 0; j < bucket.length; j++) {
     if (bucket[j][0] === k) {
       bucket[j][1] = null
+      this._count--
     }
+  }
+  if (this._count < this._limit*0.25) {
+    this._limit/=2
+    this.resize(this._limit)
   }
 };
 
